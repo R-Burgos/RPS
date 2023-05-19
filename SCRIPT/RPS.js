094a347487b9a8822c520dd22d1b127fab17e5df
Ruben Burgos <code.rburgos@gmail.com>
2023-05-19 18:07:48 -0500

    Implemented eventListeners, removed onClicks from HTML

--- SCRIPT/RPS.js
+++ SCRIPT/RPS.js
@@ -1,11 +1,14 @@
+//SCORE SETTINGS
 let score = JSON.parse(localStorage.getItem('score')) || {
   wins: 0,
   losses: 0,
   ties: 0
 };
 
+//UPDATE SCORE ON PAGE LOAD/REFRESH
 updateScoreElement();
 
+//AUTOPLAY FUNCTIONALITY
 let isAutoPlaying = false;
 let intervalID;
 
@@ -22,11 +25,13 @@ function autoPlay() {
   }
 }
 
-
+//PLAY GAME FUNCTIONALITY
 function playGame(playerMove) {
+  //RANDOM COMPUTER MOVE SELECTED
   const computerMove = pickComputerMove();
   let result = '';
 
+  //PLAYER MOVE SELECTION
   if(playerMove === 'scissors'){
     if (computerMove === 'rock') {
       result = 'You lose.';
@@ -55,6 +60,7 @@ function playGame(playerMove) {
     }
   }
 
+  //RESULTS AND SCORE UPDATE
   if (result === 'You win.') {
     score.wins += 1;
   } else if (result === 'You lose.') {
@@ -63,10 +69,13 @@ function playGame(playerMove) {
     score.ties += 1;
   }
 
+  //SAVING SCORE IN LOCALSTORAGE
   localStorage.setItem('score', JSON.stringify(score));
 
+  //SCORE UPDATED
   updateScoreElement();
 
+  //DISPLAY RESULTS AND COMPUTER/PLAYER MOVE
   document.querySelector('.js-result')
     .innerHTML = result;
 
@@ -77,11 +86,13 @@ function playGame(playerMove) {
       Computer`;
 }
 
+//UPDATE SCORE FUNCTIONALITY
 function updateScoreElement() {
   document.querySelector('.js-score')
     .innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
 };
 
+//RANDOM COMPUTER MOVE FUNCTIONALITY
 function pickComputerMove() {
   const randomNumber = Math.random();
   let computerMove = '';
@@ -94,3 +105,43 @@ function pickComputerMove() {
   }
   return computerMove;
 }
+
+//EVENT LISTENERS FOR BUTTONS------------------------
+
+//AUTOPLAY
+document.querySelector('.js-auto-play-button').addEventListener('click', () => {
+  autoPlay();
+});
+
+//RESET SCORE
+document.querySelector('.js-reset-score-button').addEventListener('click', () => {
+  score.wins = 0;
+  score. losses = 0;
+  score.ties = 0;
+  localStorage.removeItem('score');
+  updateScoreElement();
+});
+
+//GAME
+document.querySelector('.js-rock-button').addEventListener('click', () => {
+  playGame('rock');
+});
+
+document.querySelector('.js-paper-button').addEventListener('click', () => {
+  playGame('paper');
+});
+
+document.querySelector('.js-scissors-button').addEventListener('click', () => {
+  playGame('scissors');
+});
+
+//EVENT LISTENER FOR KEYDOWN
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'r') {
+    playGame('rock');
+  } else if (event.key === 'p') {
+    playGame('paper');
+  } else if (event.key === 's') {
+    playGame('scissors');
+  }
+});
